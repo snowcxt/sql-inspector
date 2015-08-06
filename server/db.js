e7e12d03b647e4bac3ed24e05ff458c728b0a134
snowcxt <snowcxt@gmail.com>
2015-08-06 10:46:57 -0700

    add mssql

---
+++ server/db.js
@@ -0,0 +1,28 @@
+var db = {};
+(function () {
+    var sql = require('mssql');
+
+    var config = {
+        user: 'admin',
+        password: 'password',
+        server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+        database: 'longford',
+
+        options: {
+            // encrypt: true // Use this if you're on Windows Azure
+        }
+    }
+
+    db.query = function (query, cb) {
+        var connection = new sql.Connection(config, function (err) {
+            if (err) return cb(err);
+
+            var request = new sql.Request(connection); // or: var request = connection.request();
+            request.query(query, function (err, recordset) {
+                if (err) return cb(err);
+
+                cb(null, recordset);
+            });
+        });
+    };
+})();
