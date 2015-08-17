3d3b54be1562da652a3d5f5fc9ed32f3cde65a12
snowcxt <snowcxt@gmail.com>
2015-08-17 11:23:33 -0700

    refactor code

--- app/client/Uploader.tsx
+++ app/client/Uploader.tsx
@@ -6,21 +6,23 @@ import Tree = require("./Tree/index");
 class Uploader extends TypedReact.Component<{
     setLogs: (logs: any[]) => void
 }, number>{
+    onChange(e){
+        var element = e.target;
+        if (element.files && element.files.length > 0) {
+            var textFile = element.files[0],
+                reader = new FileReader();
+            reader.readAsText(textFile);
+            reader.onload = () => {
+                this.props.setLogs(JSON.parse(reader.result).log);
+            };
+        }
+    }
+
     render(){
         return (
                 <span className="btn btn-default btn-file">
                     <i className="glyphicon glyphicon-open"></i>
-                    Open <input type="file" accept=".json" onChange={(e)=>{
-                var element:any = e.target;
-                if (element.files && element.files.length > 0) {
-                    var textFile = element.files[0],
-                        reader = new FileReader();
-                    reader.readAsText(textFile);
-                    reader.onload = () => {
-                        this.props.setLogs(JSON.parse(reader.result).log);
-                    };
-                }
-                    }} file-on-change="upload" />
+                    Open <input type="file" accept=".json" onChange={this.onChange} file-on-change="upload" />
                 </span>
         );
     }
