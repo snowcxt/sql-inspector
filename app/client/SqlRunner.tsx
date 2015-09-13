81d232946a1c055a0c5c286d3a13503df15d968a
snowcxt <snowcxt@gmail.com>
2015-09-12 20:53:45 -0700

    create StatementRunner.tsx

--- app/client/SqlRunner.tsx
+++ app/client/SqlRunner.tsx
@@ -1,39 +1,26 @@
 import React = require("react");
 import TypedReact = require("typed-react");
 
-import DbPicker = require("./DbPicker");
-var CodeMirror = require('codemirror');
-require('codemirror/mode/sql/sql');
-var Select = require('react-select');
-
-import DbMonitor = require("./DbMonitor");
-import DbLogs = require("../server/DbLogs");
 import EventEmitter = require("./EventEmitter");
-import Async = require('async');
+import DbPicker = require("./DbPicker");
+import DbMonitor = require("./DbMonitor");
+import StatementRunner = require("./StatementRunner");
 
 class SqlRunner extends TypedReact.Component<{
     ref: string;
     statement: string;
 }, {
         databases?: string[];
-        defaultDb?: string;
         monioredDatabases?: string[];
-        underMonitor?: boolean;
     }>{
-    private editor: CodeMirror.EditorFromTextArea;
-
     getStatement(): string {
-        return this.editor ? this.editor.getDoc().getValue() : "";
+        return (this.refs["sql-runner"] as any).getStatement();
     }
 
     getInitialState() {
         return {
             databases: [],
-            showConnector: true,
-            dbConnection: null,
-            defaultDb: false,
-            monioredDatabases: [],
-            underMonitor: false
+            monioredDatabases: []
         };
     }
 
@@ -41,78 +28,11 @@ class SqlRunner extends TypedReact.Component<{
         EventEmitter.Emitter.addListener(EventEmitter.Types.DB_CONNCTED, (databases) => {
             this.setState({ databases: databases });
         });
-
-        EventEmitter.Emitter.addListener(EventEmitter.Types.START_MONITOR, (underMonitor) => {
-            this.setState({
-                underMonitor: underMonitor
-            });
-        });
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (this.editor && nextProps.statement !== this.props.statement) {
-            this.editor.getDoc().setValue(nextProps.statement);
-        }
-    }
-
-    componentDidUpdate() {
-        if (!this.editor) {
-            var mirror: any = this.refs["statement"];
-            if (mirror) {
-                this.editor = CodeMirror.fromTextArea(mirror.getDOMNode(), {
-                    mode: "text/x-mssql",
-                    lineNumbers: true,
-                    smartIndent: true,
-                    viewportMargin: Infinity,
-                    extraKeys: {
-                        "Ctrl-Space": "autocomplete",
-                        "Ctrl-J": "autocomplete"
-                    }
-                });
-            }
-        }
-    }
-
-    runStatement() {
-        var statment = this.getStatement();
-
-        Async.series([
-            (callback) => {
-                DbLogs.setup(this.state.monioredDatabases, callback);
-            },
-            (callback) => {
-                DbLogs.runQuery(this.state.defaultDb, statment, false, callback);
-            },
-            (callback) => {
-                DbLogs.getNewLogs((err, logs: any[]) => {
-                    if (err) return callback(err, null);
-                    EventEmitter.Emitter.emit(EventEmitter.Types.LOG_CHANGED, logs);
-                    callback(null, null);
-                });
-            }
-        ], (err, recordset) => {
-            DbLogs.cleanLog(this.state.monioredDatabases, (cleanErr) => {
-                if (err)
-                    return EventEmitter.Emitter.emit(EventEmitter.Types.ERROR, err);
-                if (cleanErr)
-                    return EventEmitter.Emitter.emit(EventEmitter.Types.ERROR, cleanErr);
-                return EventEmitter.Emitter.emit(EventEmitter.Types.ERROR, "");
-            });
-        });
     }
 
     setDatabases(databases: string[]) {
         this.setState({
-            monioredDatabases: databases,
-            defaultDb: databases.length > 0 ? databases[0] : ""
-        });
-    }
-
-    onDefaultDbChange(e) {
-        var dbname = e.target.value;
-        this.setState({
-            defaultDb: dbname,
-            runnerDisabled: !dbname
+            monioredDatabases: databases
         });
     }
 
@@ -121,24 +41,7 @@ class SqlRunner extends TypedReact.Component<{
             <div>
                 <DbPicker databases={this.state.databases} setDatabases={this.setDatabases} />
                 <DbMonitor monioredDatabases={this.state.monioredDatabases}/>
-                <p className="sql-editor">
-                    <textarea ref="statement" defaultValue={this.props.statement}></textarea>
-                    </p>
-                <div className="input-group">
-                    <select className="form-control"  placeholder="Select default database ..." disabled={this.state.monioredDatabases.length <= 0} value={this.state.defaultDb} onChange={this.onDefaultDbChange}>
-                    {
-                    this.state.monioredDatabases.map((db) => {
-                        return (<option key={db}>{db}</option>);
-                    })
-                    }
-                        </select>
-                    <span className="input-group-btn">
-                        <button className="btn btn-primary" disabled={this.state.monioredDatabases.length <= 0 || this.state.underMonitor} onClick={this.runStatement}>
-                            <i className="glyphicon glyphicon-play"></i>
-                            Run
-                            </button>
-                        </span>
-                    </div>
+                <StatementRunner ref="sql-runner" statement={this.props.statement} monioredDatabases={this.state.monioredDatabases} />
                 </div>) : null;
     }
 }
---
+++ app/client/StatementRunner.tsx
@@ -0,0 +1,127 @@
+import React = require("react");
+import TypedReact = require("typed-react");
+
+import DbLogs = require("../server/DbLogs");
+import EventEmitter = require("./EventEmitter");
+import Async = require('async');
+
+var CodeMirror = require('codemirror');
+require('codemirror/mode/sql/sql');
+
+class SqlRunner extends TypedReact.Component<{
+    ref: string;
+    statement: string;
+    monioredDatabases: string[];
+}, {
+        defaultDb?: string;
+        underMonitor?: boolean;
+    }>{
+    private editor: CodeMirror.EditorFromTextArea;
+
+    getInitialState() {
+        return {
+            defaultDb: "",
+            underMonitor: false
+        };
+    }
+
+    getStatement(): string {
+        return this.editor ? this.editor.getDoc().getValue() : "";
+    }
+
+    componentDidMount() {
+        EventEmitter.Emitter.addListener(EventEmitter.Types.START_MONITOR, (underMonitor) => {
+            this.setState({
+                underMonitor: underMonitor
+            });
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            defaultDb: nextProps.monioredDatabases.length > 0 ? nextProps.monioredDatabases[0] : ""
+        });
+
+        if (this.editor && nextProps.statement !== this.props.statement) {
+            this.editor.getDoc().setValue(nextProps.statement);
+        }
+    }
+
+    componentDidUpdate() {
+        if (!this.editor) {
+            var mirror: any = this.refs["statement"];
+            if (mirror) {
+                this.editor = CodeMirror.fromTextArea(mirror.getDOMNode(), {
+                    mode: "text/x-mssql",
+                    lineNumbers: true,
+                    smartIndent: true,
+                    viewportMargin: Infinity,
+                    extraKeys: {
+                        "Ctrl-Space": "autocomplete",
+                        "Ctrl-J": "autocomplete"
+                    }
+                });
+            }
+        }
+    }
+
+    runStatement() {
+        var statment = this.getStatement();
+
+        Async.series([
+            (callback) => {
+                DbLogs.setup(this.props.monioredDatabases, callback);
+            },
+            (callback) => {
+                DbLogs.runQuery(this.state.defaultDb, statment, false, callback);
+            },
+            (callback) => {
+                DbLogs.getNewLogs((err, logs: any[]) => {
+                    if (err) return callback(err, null);
+                    EventEmitter.Emitter.emit(EventEmitter.Types.LOG_CHANGED, logs);
+                    callback(null, null);
+                });
+            }
+        ], (err, recordset) => {
+            DbLogs.cleanLog(this.props.monioredDatabases, (cleanErr) => {
+                if (err)
+                    return EventEmitter.Emitter.emit(EventEmitter.Types.ERROR, err);
+                if (cleanErr)
+                    return EventEmitter.Emitter.emit(EventEmitter.Types.ERROR, cleanErr);
+                return EventEmitter.Emitter.emit(EventEmitter.Types.ERROR, "");
+            });
+        });
+    }
+
+    onDefaultDbChange(e) {
+        var dbname = e.target.value;
+        this.setState({
+            defaultDb: dbname
+        });
+    }
+
+    render() {
+        return (<div>
+        <p className="sql-editor">
+            <textarea ref="statement" defaultValue={this.props.statement}></textarea>
+            </p>
+        <div className="input-group">
+            <select className="form-control"  placeholder="Select default database ..." disabled={this.props.monioredDatabases.length <= 0} value={this.state.defaultDb} onChange={this.onDefaultDbChange}>
+            {
+            this.props.monioredDatabases.map((db) => {
+                return (<option key={db}>{db}</option>);
+            })
+            }
+                </select>
+            <span className="input-group-btn">
+                <button className="btn btn-primary" disabled={this.props.monioredDatabases.length <= 0 || this.state.underMonitor} onClick={this.runStatement}>
+                    <i className="glyphicon glyphicon-play"></i>
+                    Run
+                    </button>
+                </span>
+            </div>
+            </div>);
+    }
+}
+
+export = TypedReact.createClass(SqlRunner);
