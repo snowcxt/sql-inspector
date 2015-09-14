d60468a57a04d54715ba3388aa74882c922714ab
snowcxt <snowcxt@gmail.com>
2015-09-13 17:04:06 -0700

    update main for auto update

--- app/main.js
+++ app/main.js
@@ -20,26 +20,21 @@ app.on('window-all-closed', function () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        title: "Sql Seer",
-        width: 1000,
-        height: 800,
-        icon: __dirname + '/icon.png'
-    });
-    // mainWindow.maximize();
-
-    // and load the index.html of the app.
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-    // Open the devtools.
-    // mainWindow.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+    updater.on('ready', function () {
+       mainWindow = new BrowserWindow({
+           width: 1000,
+       height: 800,})
+       mainWindow.loadUrl('file://' + __dirname + '/index.html')
+       mainWindow.openDevTools({detach:true})
+       mainWindow.on('closed', function() {
+           mainWindow = null;
+       })
+   })
+   updater.on('updateRequired', function () {
+       app.quit();
+   })
+   updater.on('updateAvailable', function () {
+       mainWindow.webContents.send('update-available');
+   })
+   updater.start();
 });
