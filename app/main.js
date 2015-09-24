a6df0f77f6a517fb9c8c04d2d0225ac2a90f5eaf
snowcxt <snowcxt@gmail.com>
2015-09-23 23:40:27 -0700

    publish 0.0.9

--- app/main.js
+++ app/main.js
@@ -10,35 +10,37 @@ require('crash-reporter').start();
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function () {
-    updater.on('ready', function () {
-        mainWindow = new BrowserWindow({
-            title: "Sql Seer",
-            width: 1000,
-            height: 800,
-            icon: __dirname + '/icon.png'
-        });
-        mainWindow.loadUrl('file://' + __dirname + '/index.html?version=' + app.getVersion());
+app.on('ready', function() {
+  updater.on('ready', function() {
+    mainWindow = new BrowserWindow({
+      title: "Sql Seer",
+      width: 1000,
+      height: 800,
+      icon: __dirname + '/icon.png'
+    });
+    mainWindow.loadUrl('file://' + __dirname + '/index.html?version=' + app.getVersion());
 
-        mainWindow.on('closed', function () {
-            mainWindow = null;
-        });
+    mainWindow.on('closed', function() {
+      mainWindow = null;
     });
-    updater.on('updateRequired', function () {
-        app.quit();
-    });
-    updater.on('updateAvailable', function () {
-        mainWindow.webContents.send('update-available');
-    });
-    updater.start();
+  });
+  updater.on('updateRequired', function() {
+    app.quit();
+  });
+  updater.on('updateAvailable', function() {
+    mainWindow.webContents.send('update-available');
+  });
+  updater.start(function(err) {
+    mainWindow.webContents.send('update-error', err);
+  });
 });
